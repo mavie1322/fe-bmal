d1422b4f65e4b11dae5667a09f5890ebd62d5bcf
mavie1322 <babimbilyoness@gmail.com>
2022-03-08 01:00:32 +0000

    clean up code, add more attributes on the Link component, format date

--- src/components/UserProfile.jsx
+++ src/components/UserProfile.jsx
@@ -1,58 +1,65 @@
-import React, { useEffect, useState } from 'react';
-import { Link, useParams } from 'react-router-dom';
-import { getArticles, getUserByUsername } from '../utils/api';
+import React, { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
+import { getArticles, getUserByUsername } from "../utils/api";
+import { formatDate } from "../utils/utils";
 
 export function UserProfile() {
-  const {username} = useParams();
-  const [user, setUser] = useState({username: '', name:'', avatar_url: '', })
+  const { username } = useParams();
+  const [user, setUser] = useState({ username: "", name: "", avatar_url: "" });
   const [articlesList, setArticleList] = useState([]);
 
   useEffect(() => {
     getUserByUsername(username).then((commentData) => {
-      console.log(commentData)
-      setUser(commentData)
-    })
-  }, [username])
+      setUser(commentData);
+    });
+  }, [username]);
 
   useEffect(() => {
-    getArticles('','','').then((articleList) => {
-      const articlesByUser = articleList.filter((singleArticle) => singleArticle.author === username);
+    getArticles("", "", "").then((articleList) => {
+      const articlesByUser = articleList.filter(
+        (singleArticle) => singleArticle.author === username
+      );
       setArticleList(articlesByUser);
-    })
-  }, [username])
+    });
+  }, [username]);
 
-  return <>
-  {/* user personal information */}
-    <div className='userProfile_container'>
-      <div className='userProfile_data'>
-        <h3>{user.username}</h3>
-        <img src={user.avatar_url} alt={`${user.username} avatar`}/>
-        <p>Name : {user.name}</p>
+  return (
+    <>
+      {/* user personal information */}
+      <div className='userProfile_container'>
+        <div className='userProfile_data'>
+          <h3>{user.username}</h3>
+          <img src={user.avatar_url} alt={`${user.username} avatar`} />
+          <p>Name : {user.name}</p>
+        </div>
+        <div className='userArticle'>
+          {/* articles posted by the user */}
+          <h2>Articles Posted</h2>
+          <ul>
+            {articlesList.map(
+              ({ article_id, title, comment_count, votes, created_at }) => {
+                return (
+                  <Link
+                    key={article_id}
+                    to={`/articles/${article_id}`}
+                    className='articlesList_link'>
+                    <li>
+                      <div className='articlesList_li'>
+                        <h3>{title}</h3>
+                        <section>
+                          <p>{comment_count} comments</p>
+                          <p>{votes}</p>
+                          <p>Created on {formatDate(created_at)}</p>
+                        </section>
+                      </div>
+                    </li>
+                  </Link>
+                );
+              }
+            )}
+          </ul>
+        </div>
       </div>
-      <div className='userArticle'>
-        {/* articles posted by the user */}
-        <h2>Articles Posted</h2>
-        <ul>
-          {articlesList.map(({article_id, title, comment_count, votes, created_at}) => {
-            return(
-              <Link key={article_id} to={`/articles/${article_id}`} className='articlesList_link'>
-                <li>
-                  <div className='articlesList_li'>
-                    <h3>{title}</h3>
-                    <section>
-                      <p>{comment_count} comments</p>
-                      <p>{votes}</p>
-                      <p>Created on {created_at}</p>
-                    </section>
-                  </div>
-                </li>
-              </Link>
-            )
-          })}
-        </ul>
-
-      </div>
-    </div>
-  </>;
+    </>
+  );
 }
-
