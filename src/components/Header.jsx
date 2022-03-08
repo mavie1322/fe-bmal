22d65925ea19cd4a0eceec114d1b3695541c1798
mavie1322 <babimbilyoness@gmail.com>
2022-03-08 14:32:04 +0000

    create the logout button and the function to do so

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,26 +1,43 @@
-import React, { useContext } from 'react';
-import { Link } from 'react-router-dom';
-import '../App.css'
-import { UserContext } from '../context/user';
+import React, { useContext } from "react";
+import { Link } from "react-router-dom";
+import "../App.css";
+import { UserContext } from "../context/user";
 
 export function Header() {
-  const {loggedInUser} = useContext(UserContext);
+  const { loggedInUser, setLoggedInUser } = useContext(UserContext);
+
+  const handleLogout = () => {
+    setLoggedInUser((loggedInUser) => ({
+      username: "",
+      avatar_url: "",
+      name: "",
+      auth: false,
+    }));
+  };
 
   return (
     <nav className='header_nav'>
-      <div > 
-        <Link className='header_logo' to='/'>BMAL</Link>
+      <div>
+        <Link className='header_logo' to='/'>
+          BMAL
+        </Link>
       </div>
       <div className='header_loggedUser'>
         <Link to={`/users/${loggedInUser.username}`}>
-          <div>
-            <img src={loggedInUser.avatar_url} alt={loggedInUser.name + 'avatar'} className='header_loggedUser_img'/>
-            <p>{loggedInUser.name}</p>
+          <div className='header_loggedUser_div'>
+            <img
+              src={loggedInUser.avatar_url}
+              alt={loggedInUser.name + "avatar"}
+              className='header_loggedUser_img'
+            />
+            <input
+              type='button'
+              value='Log out'
+              onClick={() => handleLogout()}
+            />
           </div>
         </Link>
       </div>
     </nav>
   );
 }
-
-
