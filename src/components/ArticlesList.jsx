a50ce01b55845c7ce73651533777291964f2fd06
mavie1322 <babimbilyoness@gmail.com>
2022-03-08 14:30:22 +0000

    format the code in the component

--- src/components/ArticlesList.jsx
+++ src/components/ArticlesList.jsx
@@ -20,33 +20,33 @@ export function ArticlesList(props) {
     <>
       <Queries sortBy={setSortBy} order={setOrder} />
       <div className='articleList_container'>
-        <ul>
-          {articlesList.map((article) => {
-            return (
-              <Link
-                key={article.article_id}
-                to={`/articles/${article.article_id}`}
-                className='articlesList_link'>
-                <li>
-                  <div className='articlesList_li'>
-                    <h3>{article.title}</h3>
-                    <span>Posted by {article.author}</span>
-                    <section>
-                      <p>{article.comment_count} comments</p>
-                      {article.votes >= 0 ? (
-                        <p className='heart'>{article.votes} ❤️</p>
-                      ) : (
-                        <p className='heart'>{article.votes} 🖤</p>
-                      )}
+        {/* <ul> */}
+        {articlesList.map((article) => {
+          return (
+            <Link
+              key={article.article_id}
+              to={`/articles/${article.article_id}`}
+              className='articlesList_link'>
+              {/* <li> */}
+              <div className='articlesList_li'>
+                <h3>{article.title}</h3>
+                <span>Posted by {article.author}</span>
+                <section>
+                  <p>{article.comment_count} comments</p>
+                  {article.votes >= 0 ? (
+                    <p className='heart'>{article.votes} ❤️</p>
+                  ) : (
+                    <p className='heart'>{article.votes} 🖤</p>
+                  )}
 
-                      <p>Created on {formatDate(article.created_at)}</p>
-                    </section>
-                  </div>
-                </li>
-              </Link>
-            );
-          })}
-        </ul>
+                  <p>Created on {formatDate(article.created_at)}</p>
+                </section>
+              </div>
+              {/* </li> */}
+            </Link>
+          );
+        })}
+        {/* </ul> */}
       </div>
     </>
   );
