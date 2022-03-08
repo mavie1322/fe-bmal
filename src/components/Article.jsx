1fbaa1858fe8e98e35eafc4fb528877879e28164
mavie1322 <babimbilyoness@gmail.com>
2022-03-08 00:50:40 +0000

    clean up the code and add buttons to increase or decrease votes

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,44 +1,37 @@
-import React, { useContext } from 'react';
-import { Link, useParams } from 'react-router-dom';
-import { useEffect, useState } from 'react/cjs/react.development';
-import { UserContext } from '../context/user';
-import { getArticleById, getCommentsByArticleId, updateVoteByArticle, updateVoteByComment } from '../utils/api';
-import { Comment } from './Comment';
+import React from "react";
+import { Link, useParams } from "react-router-dom";
+import { useEffect, useState } from "react/cjs/react.development";
+import { getArticleById, updateVoteByArticle } from "../utils/api";
+import { formatDate } from "../utils/utils";
+import { Comments } from "./Comments";
 
 export function Article() {
-  const {article_id} = useParams();  
-  const {loggedInUser} = useContext(UserContext);
+  const { article_id } = useParams();
   const [article, setArticle] = useState({});
-  const [comments, setComments] = useState([]);
-  const [votes, setVotes] = useState()
-  const [commentId, setCommentId] = useState()
-  const [isOpen, setIsOpen] = useState(false)
-
 
   const increaseArticleVotes = () => {
     article.votes += 1;
     setArticle((currentArticle) => {
-      let newArticle = {...currentArticle, votes: article.votes};
+      let newArticle = { ...currentArticle, votes: article.votes };
       return newArticle;
-    })
-    updateVoteByArticle(article_id, {votes: 1})
+    });
+    updateVoteByArticle(article_id, { votes: 1 });
   };
 
   const decreaseArticleVotes = () => {
-     article.votes -= 1;
+    article.votes -= 1;
     setArticle((currentArticle) => {
-      let newArticle = {...currentArticle, votes: article.votes};
+      let newArticle = { ...currentArticle, votes: article.votes };
       return newArticle;
-    })
-    updateVoteByArticle(article_id, {votes: -1})
-  }
+    });
+    updateVoteByArticle(article_id, { votes: -1 });
+  };
 
   useEffect(() => {
     getArticleById(article_id).then((article) => {
-      setArticle(article)
-    })
-  }, [article_id])
-
+      setArticle(article);
+    });
+  }, [article_id]);
 
   return (
     <>
@@ -49,20 +42,25 @@ export function Article() {
               <h6>{article.author}</h6>
             </Link>
           </div>
-          <p className='article_date'>Posted on {article.created_at}</p>
+          <p className='article_date'>
+            Posted on {formatDate(article.created_at)}
+          </p>
           <p className='article_hashtag'>#{article.topic}</p>
           <h2>{article.title}</h2>
           <article>{article.body}</article>
           <div className='vote'>
-            <button type='button' onClick={() => increaseArticleVotes()}>	&#128077;</button>
+            <button type='button' onClick={() => increaseArticleVotes()}>
+              {" "}
+              &#128077;
+            </button>
             <p>{article.votes}</p>
-            <button type='button' onClick={() => decreaseArticleVotes()}>&#128078;</button>
+            <button type='button' onClick={() => decreaseArticleVotes()}>
+              &#128078;
+            </button>
           </div>
         </section>
-          <Comment article_id={article_id}/> 
+        <Comments article_id={article_id} />
       </div>
     </>
   );
 }
-
-
