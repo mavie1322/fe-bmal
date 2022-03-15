fef7195b778cf4b37a33f920abd96d7a0dbb7bc9
mavie1322 <babimbilyoness@gmail.com>
2022-03-15 19:09:14 +0000

    transfer the functionality to delete a comment from the Comments component to the Popup

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -1,13 +1,30 @@
 import React from "react";
+import { deleteCommentOnArticle } from "../utils/api";
 
-const Popup = (props) => {
+const Popup = ({ handleClose, setComments, commentId }) => {
+  const deleteComment = (comment_id) => {
+    setComments((currCommentList) => {
+      const newCommentList = currCommentList.filter(
+        (singleComment) => singleComment.comment_id !== comment_id
+      );
+      return newCommentList;
+    });
+    handleClose(comment_id);
+    deleteCommentOnArticle(comment_id);
+  };
+
   return (
     <div className='popup-box'>
       <div className='box'>
-        <span className='close-icon' onClick={props.handleClose}>
+        <span className='close-icon' onClick={handleClose}>
           x
         </span>
-        {props.content}
+        <div className='delete_container'>
+          <h3>Are you sure you want to delete this comment?</h3>
+          <button type='button' onClick={() => deleteComment(commentId)}>
+            Delete
+          </button>
+        </div>
       </div>
     </div>
   );
