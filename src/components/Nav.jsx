38b8c2dfe04c638d78348620158afce24a49a6a8
mavie1322 <babimbilyoness@gmail.com>
2022-03-16 12:29:09 +0000

    import correctly useEffect from react

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,35 +1,42 @@
-import React, { useState } from 'react';
-import { useEffect } from 'react/cjs/react.development';
-import { getTopics } from '../utils/api';
-import { ArticlesList } from './ArticlesList';
+import React, { useState } from "react";
+import { useEffect } from "react";
+import { getTopics } from "../utils/api";
+import { ArticlesList } from "./ArticlesList";
 
 export function Nav() {
   const [topics, setTopics] = useState([]);
-  const [pickedTopic, setPickedTopic] = useState('');
+  const [pickedTopic, setPickedTopic] = useState("");
 
   const handleClick = (clickedTopic) => {
     setPickedTopic(clickedTopic);
   };
 
-  useEffect (() => {
+  useEffect(() => {
     getTopics().then((topicsApi) => {
       setTopics(topicsApi);
-    })
+    });
   }, [pickedTopic]);
 
   return (
     <>
-    <div className='nav'>
-    <ul className='nav_topics_ul'>
-      <li onClick={() => handleClick('')} className='nav_topics_li'>All</li>
-    {topics.map((topic) => {
-      return <li key={topic.slug} onClick={() => handleClick(topic.slug)} className='nav_topics_li'>{topic.slug}</li>
-    })}
-    </ul>
-    </div>
-    <ArticlesList topic={pickedTopic}/>
+      <div className='nav'>
+        <ul className='nav_topics_ul'>
+          <li onClick={() => handleClick("")} className='nav_topics_li'>
+            All
+          </li>
+          {topics.map((topic) => {
+            return (
+              <li
+                key={topic.slug}
+                onClick={() => handleClick(topic.slug)}
+                className='nav_topics_li'>
+                {topic.slug}
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+      <ArticlesList topic={pickedTopic} />
     </>
   );
-
 }
-
